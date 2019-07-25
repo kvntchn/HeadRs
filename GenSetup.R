bdd8f0111abed25309b60ee12d038846d27c81b8
Kevin Chen <kevchen@BERKELEY.EDU>
2019-07-24 17:17:17 -0700

    Clean up old gensteup.R"

--- GenSetup.R
+++ GenSetup.R
@@ -20,41 +20,6 @@ library(magrittr)
 # 				# legend.position="none"
 # 	)
 
-# Libraries
-#to load dependencies, add to required packages, then run the `lapply` statement below
-
-# requiredPackages <- c('tidyverse','xtable','pander','latex2exp','Hmisc','scales','foreign','extrafont','graphics','tikzDevice','MASS','pracma', 'mda','tree','car','leaps','glmnet','corrplot','DMwR','diagram','DiagrammeR','dagR','vistime','metafor','jsonlite','devtools')
-
-#function for package installation - checks if package exists, then installs if not, then loads for session
-packageInstaller <- function(packName) {
-	if(require(packName, character.only = TRUE)){
-		sprintf("%s is loaded correctly", packName)
-	} else {
-		sprintf("trying to install %s", packName)
-		install.packages(packName)
-		if(require(packName, character.only = TRUE)){
-			sprintf("%s installed and loaded", packName)
-		} else {
-			stop(sprintf("could not install %s", packName))
-		}
-	}
-}
-
-# lapply(requiredPackages, packageInstaller)
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Neat knitting
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-library(xtable)
-library(pander); panderOptions('missing', '')
-library(latex2exp)
-# knitr::opts_chunk$set(#echo = FALSE,
-# 	fig.align='center',
-# 	warning=FALSE,
-# 	#eval = FALSE,
-# 	fig.height=4,
-# 	fig.width=5)
-
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # High-level graphics
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -76,20 +41,6 @@ dot2tex <- function(x,engine='ftikz') {
 	system(paste0('pdflatex ', root, '.tex'))
 	system(paste0('rm ', root, c('.aux', '.tex.tex', '.log'),collapse = '; '))
 }
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Statistical learning
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# library(MASS)
-# library(pracma)
-# library(mda)
-# library(class)
-# library(tree)
-# library(car)
-# library(leaps)
-# library(glmnet)
-# library(corrplot)
-# library(DMwR)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Diagramming needs
